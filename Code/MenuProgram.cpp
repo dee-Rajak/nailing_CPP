67f739742709a4733ac6092fea4c28337cfa3177
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2022-12-26 17:36:07 +0530

    Made changes in MenuProgram

--- Code/MenuProgram.cpp
+++ Code/MenuProgram.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout<<"1. Fries\n"\
-    <<"2. Burger\n"\
-    <<"3. Shakes"<<std::endl;
-
-    int choice;
-
-    std::cout<<"Enter your choice : ";
-    std::cin>>choice;
-
-    switch (choice)
+int main()
+{
+    bool menuIsRunning = true;
+    while (menuIsRunning)
     {
-    case 1:
-        std::cout<<"Fries costs $5"<<std::endl;
-        break;
-    case 2:
-        std::cout<<"Burger costs $15"<<std::endl;
-        break;
-    case 3:
-        std::cout<<"Shake costs $10"<<std::endl;
-        break;
-    default:
-        std::cout<<"Invalid Choice"<<std::endl;
-        break;
+        std::string input;
+        int choice;
+
+        std::cout
+            << "1. Fries\n"
+            << "2. Burger\n"
+            << "3. Shakes" << std::endl;
+
+        std::cout << "Enter your choice : ";
+        getline(std::cin, input);
+        choice = std::stoi(input);
+
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Fries costs $5" << std::endl;
+            break;
+        case 2:
+            std::cout << "Burger costs $15" << std::endl;
+            break;
+        case 3:
+            std::cout << "Shake costs $10" << std::endl;
+            break;
+        default:
+            std::cout << "Invalid Choice" << std::endl;
+            break;
+        }
     }
+
     return 0;
 }
